48bd7cc4a17c25f3308a332935c292af671454e2
phthallo <84078890+phthallo@users.noreply.github.com>
2024-06-28 19:23:07 +0930

    add: preliminary work on health system

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ https://sprig.hackclub.com/gallery/getting_started
 @addedOn: 2024-00-00
 */
 
+const mask = "m"
+const maskLost = "d"
 const player = "p"
 const stone = "s"
 const background = "b"
-const mask = "m"
-const maskLost = "d"
 const screenTransitionTop = "i"
 const screenTransitionLeft = "j"
 const screenTransitionRight = "l"
@@ -29,7 +29,7 @@ var lives = 3
 
 
 setLegend(
-  [ player, bitmap`
+  [player, bitmap`
 ................
 ..000......000..
 ..020......020..
@@ -45,42 +45,43 @@ setLegend(
 ....0LLLLLL0....
 ....0LLLLLL0....
 ....00LLLL00....
-.....00LL00.....` ],
-  [ mask, bitmap `
+.....00LL00.....`],
+  [maskLost, bitmap `
 ................
 ................
-....11111111....
-...1111111111...
-...1111111111...
-...1111111111...
-...11LL11LL11...
-...11LL11LL11...
-...11LL11LL11...
-...1111111111...
-...1111111111...
-...1111111111...
-....11111111....
-......1111......
-................
-................`],
-  [ maskLost, bitmap `
-................
-................
-....11111101....
-...1111000011...
-...1111001111...
-...1111001110...
-...1111000000...
-...1110000000...
-...1100000111...
-...0001100111...
-...0111110111...
-...1111110001...
-....11111110....
-......1111......
+....00000000....
+...0011000000...
+...0111001110...
+...0111001110...
+...0111000000...
+...0110000000...
+...0100000110...
+...0001100110...
+...0111110110...
+...0011110000...
+....00011000....
+......0000......
 ................
 ................`],
-  [ screenTransitionTop, bitmap `
+
+  [mask, bitmap `
+................
+.....000000.....
+....00000000....
+...0111111110...
+...0111111110...
+...0111111110...
+...01LL11LL10...
+...01LL11LL10...
+...01LL11LL10...
+...0111111110...
+...0111111110...
+...0011111100...
+....00011000....
+......0000......
+.......00.......
+................`],
+  [screenTransitionTop, bitmap `
 LLLLLLLLLLLLLLLL
 LLLLLLLLLLLLLLLL
 LLLLLLLLLLLLLLLL
@@ -97,7 +98,7 @@ LLLLLLLLLLLLLLLL
 ................
 ................
 ................`],
-  [ screenTransitionLeft, bitmap `
+  [screenTransitionLeft, bitmap `
 LLL.............
 LLL.............
 LLL.............
@@ -114,7 +115,7 @@ LLL.............
 LLL.............
 LLL.............
 LLL.............`],
-  [ screenTransitionRight, bitmap `
+  [screenTransitionRight, bitmap `
 .............LLL
 .............LLL
 .............LLL
@@ -131,7 +132,7 @@ LLL.............`],
 .............LLL
 .............LLL
 .............LLL`],
-  [ screenTransitionBottom, bitmap `
+  [screenTransitionBottom, bitmap `
 ................
 ................
 ................
@@ -148,7 +149,7 @@ LLL.............`],
 LLLLLLLLLLLLLLLL
 LLLLLLLLLLLLLLLL
 LLLLLLLLLLLLLLLL`],
-  [ stone, bitmap `
+  [stone, bitmap `
 1111111111111111
 1111111111LL1111
 11LLLLL111LL11L1
@@ -165,7 +166,7 @@ LL1LL1000LL11111
 LLLL11111110LLL1
 1LLL11LL1100LLL1
 1LLLL1111LL11111`],
-  [ background, bitmap `
+  [background, bitmap `
 1111111111111111
 1111111111111111
 1111111111111111
@@ -182,7 +183,7 @@ LLLL11111110LLL1
 1111111111111111
 1111111111111111
 1111111111111111`],
-  [ grass, bitmap `
+  [grass, bitmap `
 DDDDDDDDDDDDDDDD
 DDDDDDDDDDDDDDDD
 DDDDDDDDDDDDDDDD
@@ -199,7 +200,7 @@ DDDDDDDDDDDDDDDD
 DDDDDDDDDDDDDDDD
 DDDDDDDDDDDDDDDD
 DDDDDDDDDDDDDDDD`],
-  [ backgroundGrass, bitmap `
+  [backgroundGrass, bitmap `
 4444444444444444
 4444444444444444
 4444444444444444
@@ -216,7 +217,7 @@ DDDDDDDDDDDDDDDD`],
 4444444444444444
 4444444444444444
 4444444444444444`],
-  [ acid, bitmap `
+  [acid, bitmap `
 FFFFFFFFFFFFFFFF
 FFFFFFFFFFFFFFFF
 FFFFFFFFFFFFFFFF
@@ -291,104 +292,129 @@ ggggggggggggg`
 ]
 
 const levelsDir = {
-     "R0":[[2, 9, 7], null, null, [1, 12, 1]],
-     "R1":[[0, 9, 7], null, null, null], 
-     "R2":["", [3, 10, 6], null, [0, 10, 2]],
-     "R3":[null, "", [2, 1, 6], [4, 2, 1]],
-     "R4":[[3, 4, 7], null, null, null],
+  "R0": [
+    [2, 9, 7], null, null, [1, 12, 1]
+  ],
+  "R1": [
+    [0, 9, 7], null, null, null
+  ],
+  "R2": ["", [3, 10, 6], null, [0, 10, 2]],
+  "R3": [null, "", [2, 1, 6],
+    [4, 2, 1]
+  ],
+  "R4": [
+    [3, 4, 7], null, null, null
+  ],
 };
 
 setMap(levels[level])
 
 setPushables({
-  [ player ]: []
+  [player]: []
 })
+
+// generate masks
+updateHealth()
 
 
 // ceiling is above head check
-function ceilingCheck(sprite){
+function ceilingCheck(sprite) {
   var yPos = sprite.y
   sprite.y -= movement // try to move the sprite up
-  if (sprite.y == yPos){ // if moving up didnt work
+  if (sprite.y == yPos) { // if moving up didnt work
     return true
   } else // uh oh move it back
     sprite.y += movement
-    return false
+  return false
 }
 
 // is on ground check
-function groundCheck(sprite){
+function groundCheck(sprite) {
   var yPos = sprite.y
   sprite.y += movement // try to move the sprite down
-  if (sprite.y == yPos){
+  if (sprite.y == yPos) {
     // do nothing because apparently javascript doesnt have pass
     return true
-  } else 
+  } else
     sprite.y -= movement // wow the sprite is not currently on the ground! move it back
-    return false
+  return false
 }
 
-function moveDown(sprite){
-    setTimeout(() => { 
-      while (!(groundCheck(sprite))){
-          sprite.y += movement;
-      }
-    }, 300);
+function moveDown(sprite) {
+  setTimeout(() => {
+    while (!(groundCheck(sprite))) {
+      sprite.y += movement;
+    }
+  }, 300);
 }
-  
 
-function checkInteraction(sprite1, lvl){
+
+function checkInteraction(sprite1, lvl) {
   const arr = ["i", "j", "l", "k"]
   knightCoords = getTile(getFirst(sprite1).x, getFirst(sprite1).y)
   currentLevel = "R" + lvl
-  for (roomDirection in [0,1,2,3]){
-    if (knightCoords.find(sprite => sprite.type == arr[roomDirection]) && (levelsDir[currentLevel][roomDirection])){
+  for (roomDirection in [0, 1, 2, 3]) {
+    if (knightCoords.find(sprite => sprite.type == arr[roomDirection]) && (levelsDir[currentLevel][roomDirection])) {
       roomInfo = levelsDir[currentLevel][roomDirection]
       level = roomInfo[0]
       //console.log("respawning in" + roomInfo[0] + "at" + roomInfo[1] + "," + roomInfo[2])
       setMap(levels[level])
       getFirst(sprite1).x = roomInfo[1]
       getFirst(sprite1).y = roomInfo[2]
-        }
+      updateHealth()
+    }
   }
 }
 
-function checkHazard(sprite, hazard, respawnCoords){
-    knightCoords = getTile(getFirst(sprite).x, (getFirst(sprite).y))
-    if (knightCoords.find(sprite => sprite.type == hazard)){
-      lives -= 1
-      console.log(lives)
-      getFirst(sprite).x = respawnCoords[0]
-      getFirst(sprite).y = respawnCoords[1]
+function checkHazard(sprite, hazard, respawnCoords) {
+  knightCoords = getTile(getFirst(sprite).x, (getFirst(sprite).y))
+  if (knightCoords.find(sprite => sprite.type == hazard)) {
+    lives -= 1
+    updateHealth()
+    console.log(lives)
+    getFirst(sprite).x = respawnCoords[0]
+    getFirst(sprite).y = respawnCoords[1]
+  }
+}
+
+function updateHealth(){
+  for (let i = 0; i < 3; i++){ // 
+    for (let j = 0; j < lives; j++){
+      addSprite(j,0, mask)
     }
+      addSprite(3-lives,0, maskLost)
+  }
+
 }
 
 // Directions or smth yay
 onInput("w", () => {
-  if (!(ceilingCheck(getFirst(player)))){
-      getFirst(player).y -= movement;
-      getFirst(player).y -= movement;
-      moveDown(getFirst(player))
+  if (!(ceilingCheck(getFirst(player)))) {
+    getFirst(player).y -= movement;
+    getFirst(player).y -= movement;
+    moveDown(getFirst(player))
   }
 })
 
 onInput("a", () => {
   getFirst(player).x -= movement;
-  if (!(groundCheck(getFirst(player)))){
-      moveDown(getFirst(player))
-  }})
+  if (!(groundCheck(getFirst(player)))) {
+    moveDown(getFirst(player))
+  }
+})
 
 
 
 onInput("d", () => {
   getFirst(player).x += movement;
-  if (!(groundCheck(getFirst(player)))){
-      moveDown(getFirst(player))
-  }})
+  if (!(groundCheck(getFirst(player)))) {
+    moveDown(getFirst(player))
+  }
+})
 
 
 afterInput(() => {
   console.log(getFirst(player).x, getFirst(player).y)
   checkInteraction(player, level)
-  checkHazard(player, acid, [7,1])
+  checkHazard(player, acid, [8, 3])
 })
